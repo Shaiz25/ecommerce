e992126f64e7cbce8241c55165562b764ac252bb
Shaiz25 <sanvishailja@gmail.com>
2025-06-24 20:54:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,23 +13,7 @@ export default defineConfig({
 			},
 		},
 	},
-	import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(),tailwindcss()],
-	
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				
-			},
-		},
-	},
-	  build: {
+	 build: {
     outDir: 'dist', // Optional: specify if framework/tool expects this
   }
-});
 });

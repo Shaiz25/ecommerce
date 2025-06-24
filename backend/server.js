a57dafa0e2b88a72dd876cc7e38d71b42acaf25c
Shaiz25 <sanvishailja@gmail.com>
2025-06-24 20:37:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,13 @@ app.use(
     next();
 });
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 
